d84dd4f9cd1b6df01126f05f21893f633598a6ac
Milton Restrepo <miltonjrv@gmail.com>
2023-01-20 20:35:19 -0500

    Ejercicio MinimalApi, EF con PostgreSQL

---
+++ MinimalAPIPosgreSQLEF/Data/OfficeDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPosgreSQLEF.Models;
+
+namespace MinimalAPIPosgreSQLEF.Data
+{
+    public class OfficeDb : DbContext
+    {
+        public OfficeDb(DbContextOptions<OfficeDb> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
